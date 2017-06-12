d3f357c7c5291f2b3cbdf7efcb9c2e297afcaa58
James Lenehan (MAC) <jameslenehan@icloud.com>
2017-06-13 09:40:39 +1000

    setup created

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='TrackingProject',
+    version='',
+    packages=['Server', 'Server.Core', 'Server.WebUI', 'Server.IODevice', 'Server.ScoringEngine', 'SysAdmin'],
+    url='',
+    license='',
+    author='jameslenehan',
+    author_email='',
+    description=''
+)
